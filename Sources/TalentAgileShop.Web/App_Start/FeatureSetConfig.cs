097f3ecb60b4d6205727e9d53f628e2858bba9e9
Michel Perfetti <miiitch@gmail.com>
2017-03-01 09:19:29 +0100

    Allow featureSet configured by web site env variables

--- Sources/TalentAgileShop.Web/App_Start/FeatureSetConfig.cs
+++ Sources/TalentAgileShop.Web/App_Start/FeatureSetConfig.cs
@@ -1,3 +1,4 @@
+using System;
 using TalentAgileShop.Model;
 
 namespace TalentAgileShop.Web
@@ -11,6 +12,28 @@ namespace TalentAgileShop.Web
         {
             featureSet.ThumbnailView = false;
             featureSet.CatalogCategories = false;
+
+            OverrideWithEnvVariableConfig(featureSet);
+        }
+
+        private static void OverrideWithEnvVariableConfig(FeatureSet featureSet)
+        {
+            featureSet.ThumbnailView = GetBoolValueFromEnv("EnableThumbnailView", featureSet.ThumbnailView);
+            featureSet.CatalogCategories = GetBoolValueFromEnv("EnableCatalogCategories", featureSet.CatalogCategories);
+
+        }
+
+        private static bool GetBoolValueFromEnv(string environmentVariable, bool defaultValue)
+        {
+            var envValue = System.Environment.GetEnvironmentVariable(environmentVariable);
+
+            if (envValue == null)
+            {
+                return defaultValue;                
+            }
+
+            return string.Compare(envValue.Trim(), "true", StringComparison.InvariantCultureIgnoreCase) == 0;
+
         }
     }
 }
