5b10507fc44d6d3a02023092aea38aa57bd408d9
Michel Perfetti <miiitch@gmail.com>
2017-03-13 13:47:11 +0100

    DB project imported

---
+++ Sources/TalentAgileStop.Database/dbo/Tables/Categories.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Categories] (
+    [Id]   NVARCHAR (128) NOT NULL,
+    [Name] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_dbo.Categories] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Sources/TalentAgileStop.Database/dbo/Tables/Countries.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Countries] (
+    [Id]   NVARCHAR (128) NOT NULL,
+    [Name] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_dbo.Countries] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
